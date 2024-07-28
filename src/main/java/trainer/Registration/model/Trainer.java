47ba5c84b5c337d9cb84ea3d0334028db5b5be9b
Ash <Ash@DESKTOP-6QI1BR5>
2024-07-29 01:08:15 +0530

    h

---
+++ src/main/java/trainer/Registration/model/Trainer.java
@@ -0,0 +1,62 @@
+package trainer.Registration.model;
+
+public class Trainer {
+
+	private String firstName;
+	private String lastName;
+	private String EmailAddress;
+	private String PhoneNo;
+	private String Dob;
+	private String gender;
+	private String username;
+	private String Service;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmailAddress() {
+		return EmailAddress;
+	}
+	public void setEmailAddress(String emailAddress) {
+		EmailAddress = emailAddress;
+	}
+	public String getPhoneNo() {
+		return PhoneNo;
+	}
+	public void setPhoneNo(String phoneNo) {
+		PhoneNo = phoneNo;
+	}
+	public String getDob() {
+		return Dob;
+	}
+	public void setDob(String dob) {
+		Dob = dob;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getService() {
+		return Service;
+	}
+	public void setService(String service) {
+		Service = service;
+	}
+	
+}
